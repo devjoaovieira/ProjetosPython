77cfadc4013229ecd642a7194efb08a992ae136f
devjoaovieira <joaovieiraneto3@gmail.com>
2025-10-11 09:48:50 -0300

    testando e criando dicionário alunos

---
+++ PROJ_SIST_GEN_NOTAS_ACAD/main.py
@@ -0,0 +1,10 @@
+#criar lista turma
+#criar dicionario ALUNOS - cada dicionario terá o nome do aluno e suas respectivas notas
+turmas = []
+aluno1 = { "nome": "João", "nota de Português": 8.0, "nota de Matemática": 9.0, "nota de Física": 7.5} 
+aluno2 = {"nome": "Beatriz", "nota de Português": 6.0, "nota de Matemática": 5.5, "nota de Física": 9.5}
+aluno3 = {"nome": "Carlos", "nota de Português": 10.0, "nota de Matemática": 5.0, "nota de Física": 4.0}
+turmas.append(aluno1)
+turmas.append(aluno2)
+turmas.append(aluno3)
+print(turmas)
