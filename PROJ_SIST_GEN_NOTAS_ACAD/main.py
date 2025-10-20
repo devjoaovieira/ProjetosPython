c3c5508cd3acca482bff88468089ab64fc6d2d0c
devjoaovieira <joaovieiraneto3@gmail.com>
2025-10-19 21:53:40 -0300

    feat: apagando dicionário, criando função de validação numérica e função de menu principal. adicionando opção de inserir aluno - O dicionário correspondente a cada aluno sera representado pelo seu número de matrícula. A turma será representada por uma lista.

--- PROJ_SIST_GEN_NOTAS_ACAD/main.py
+++ PROJ_SIST_GEN_NOTAS_ACAD/main.py
@@ -1,10 +1,42 @@
-#criar lista turma
-#criar dicionario ALUNOS - cada dicionario terá o nome do aluno e suas respectivas notas
-turmas = []
-aluno1 = { "nome": "João", "nota de Português": 8.0, "nota de Matemática": 9.0, "nota de Física": 7.5} 
-aluno2 = {"nome": "Beatriz", "nota de Português": 6.0, "nota de Matemática": 5.5, "nota de Física": 9.5}
-aluno3 = {"nome": "Carlos", "nota de Português": 10.0, "nota de Matemática": 5.0, "nota de Física": 4.0}
-turmas.append(aluno1)
-turmas.append(aluno2)
-turmas.append(aluno3)
-print(turmas)
+#criar função auxiliar para validar entradas numéricas dos usuários
+def entrada_numerica(mensagem):
+    while True:
+            try:
+                numero = int(input('Escolha um número: '))
+                return numero
+            except ValueError:
+                print("Erro: Você não digitou um número válido! \nTente novamente!")
+#-----------------------------------------------------------------------------------#
+def exibir_menu():
+    print('GERENCIADOR DE NOTAS ACADÊMICAS\n')
+    print('1. Adicionar Novo Aluno')
+    print('2. Lançar Notas do Aluno')
+    print('3. Visualizar Boletim')
+    print('4. Sair')
+
+    opcao = entrada_numerica("Escolha uma opção entre 1 e 4: ")
+    return opcao
+#-----------------------------------------------------------------------------------#
+def main():
+    turma = []
+    while True:
+        opcao = exibir_menu()
+        if opcao == 1:
+            matricula = int(input("Matricula do aluno: "))
+            matricula = {}
+            matricula['nome'] = str(input('Digite o nome do aluno: '))
+            matricula['curso'] = str(input(f'Digite o nome do curso de {matricula['nome']}: ')).strip().capitalize()
+            turma.append(matricula)
+            print(matricula)
+            print(turma)
+            
+            
+            #perguntar nome
+            #matricula 6 digitos
+            #curso
+    
+            
+
+#codigo principal
+
+main()
